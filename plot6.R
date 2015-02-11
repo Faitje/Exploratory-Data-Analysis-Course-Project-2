397de582586b1c9f63dc969029f1c36371639f7e
Fai <fai.greeve@gmail.com>
2015-02-11 16:31:50 +0100

    Changes Plot 6

--- plot6.R
+++ plot6.R
@@ -15,7 +15,6 @@ SCC <- readRDS("Source_Classification_Code.rds")
 motor <- subset(SCC, grepl("Motor", Short.Name))
 NEImotor2 <- subset(NEIsub2, SCC %in% motor$SCC)
 
-
 NEIagg5 <- aggregate(NEImotor2$Emissions, list(year = NEImotor2$year, county = NEImotor2$fips), sum)
 NEIagg5$county <- recode(NEIagg5$county,"'24510'='Los Angeles County';'06037'='Baltimore City'")
 
@@ -23,14 +22,13 @@ NEIagg5$county <- recode(NEIagg5$county,"'24510'='Los Angeles County';'06037'='B
 png(filename='plots/plot6.png')
 
 # Make plot
-ggplot(NEIagg5, aes(x=year, y=x, fill = county)) +
+ggplot(NEIagg5, aes(x=factor(year), y=x, fill = county)) +
         geom_bar(stat="identity") + 
         guides(fill=FALSE) +
         theme_bw() +
         facet_grid(county ~ .) +
         labs(list(title = 'Total PM2.5 emission from motor vehicles \n for Baltimore City (MD) & Los Angeles County (CA) by year', 
-                  y = "Emission PM2.5 (tons)"
-                  , x = " "))
+                  y = "Emission PM2.5 (tons)", x = " "))
 
 # Turn off PNG device
 dev.off()
